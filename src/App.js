fab5d0d6ccbc70cfd78621b860d077c14c2249d2
KKShakya <krishnashakya7272@gmail.com>
2022-12-04 17:15:45 +0530

    ci/cd github actions

--- src/App.js
+++ src/App.js
@@ -1,23 +1,15 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
 
 function App() {
+  const [count, setCount] = useState(0);
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      {count}
+      <div>
+        <button onClick={() => setCount(count + 1)}></button>
+        <button onClick={() => setCount(count - 1)}></button>
+      </div>
     </div>
   );
 }
